82fa55cb54dc7ed0624e828c64d5e988a0fad2be
Mootbing <50122069+Mootbing@users.noreply.github.com>
2022-06-01 21:42:35 -0400

    Patch1

--- SRC/Handlers/UI/DashboardPane.py
+++ SRC/Handlers/UI/DashboardPane.py
@@ -21,7 +21,7 @@ class DashboardPane(TemplatePane):
 
     DirectoryAbove = "/".join(os.path.dirname(os.path.realpath(__file__)).replace("\\", "/").split("/")[:-2])
 
-    def __init__(self, Root, SettingsHandler, width=400, height=50, bg = "#2D2D2D"):
+    def __init__(self, Root, SettingsHandler, width=400, height=50, bg = Usefuls.LightGrey):
         super().__init__(Root, SettingsHandler, width, height)
 
         self.Root = Root
@@ -83,7 +83,7 @@ class DashboardPane(TemplatePane):
 
         self.ProjectPane = tk.Canvas(self.Frame, width=720-142, height=512-150, bg=self.Background, highlightthickness=0)
 
-        self.BitesBackgroundText = tk.Label(self.ProjectPane, text="Unrealify Projects", font=("Yu Gothic Bold", 24), bg=self.Background, foreground="#FFF")
+        self.BitesBackgroundText = tk.Label(self.ProjectPane, text="Unrealify Projects", font=(Usefuls.FontAccented, 24), bg=self.Background, foreground=Usefuls.White)
         self.BitesBackgroundText.grid(pady=10)
 
         self.BrowserPane = ScrollPane(self.ProjectPane, self.Background, 720-167, 512-150)
@@ -118,9 +118,9 @@ class DashboardPane(TemplatePane):
 
         self.Add(self.ProjectPane, 10, (10, 0))
 
-        self.BottomPane = tk.Canvas(self.Frame, width=720-142, height=75, bg="#121212", highlightthickness=0)
+        self.BottomPane = tk.Canvas(self.Frame, width=720-142, height=75, bg=Usefuls.LightBlack, highlightthickness=0)
 
-        self.Pather = tk.Text(self.BottomPane, bg="#2D2D2D", foreground="#FFF", font=("Yu Gothic", 12), borderwidth=0, highlightthickness=0)
+        self.Pather = tk.Text(self.BottomPane, bg=Usefuls.LightGrey, foreground=Usefuls.White, font=(Usefuls.Font, 12), borderwidth=0, highlightthickness=0)
         self.Pather.place(x=5, y=10, width=505, height=25)
 
         if self.DataParser.GetAllData()["LastLeft"] != "":
@@ -138,16 +138,16 @@ class DashboardPane(TemplatePane):
             self.Pather.delete("1.0", tk.END)
             self.Pather.insert(tk.INSERT, str(File))
 
-        self.OpenExplorerButton = tk.Button(self.BottomPane, text="...", bg="#292929", foreground="#FFF", font=("Yu Gothic", 14), borderwidth=0, highlightthickness=0, command=SetFile)
+        self.OpenExplorerButton = tk.Button(self.BottomPane, text="...", bg=Usefuls.LightGrey, foreground=Usefuls.White, font=(Usefuls.Font, 14), borderwidth=0, highlightthickness=0, command=SetFile)
         self.OpenExplorerButton.place(x=500, y=10, width=50, height=25, anchor="nw")
 
-        self.ButtonCanvas = tk.Canvas(self.BottomPane, bg="#121212", highlightthickness=0)
+        self.ButtonCanvas = tk.Canvas(self.BottomPane, bg=Usefuls.LightBlack, highlightthickness=0)
         self.ButtonCanvas.place(relx=1, rely=0.875, anchor="se")
 
-        self.OpenButton = tk.Button(self.ButtonCanvas, text="Open", bg="#292929", foreground="#FFF", font=("Yu Gothic", 10), borderwidth=0, highlightthickness=0, command=lambda : [self.OpenProject()])
+        self.OpenButton = tk.Button(self.ButtonCanvas, text="Open", bg=Usefuls.LightGrey, foreground=Usefuls.White, font=(Usefuls.Font, 10), borderwidth=0, highlightthickness=0, command=lambda : [self.OpenProject()])
         self.OpenButton.grid(row=0, column=1, padx=(0, 10))
 
-        self.CreateButton = tk.Button(self.ButtonCanvas, text="Properties", bg="#292929", foreground="#FFF", font=("Yu Gothic", 10), borderwidth=0, highlightthickness=0)
+        self.CreateButton = tk.Button(self.ButtonCanvas, text="Properties", bg=Usefuls.LightGrey, foreground=Usefuls.White, font=(Usefuls.Font, 10), borderwidth=0, highlightthickness=0)
         self.CreateButton.grid(row=0, column=2, padx=(0, 30))
 
         self.Add(self.BottomPane, Padx=10)
